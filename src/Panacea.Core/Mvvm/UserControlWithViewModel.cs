8767f22b79fd28752d59ac51e5b4b7bf61403927
Portno <j.remoundos@dotbydot.gr>
2019-05-13 12:27:22 +0300

    added mvvm

---
+++ src/Panacea.Core/Mvvm/UserControlWithViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Panacea.Core.Mvvm
+{
+    public abstract class UserControlWithViewModel<T>:UserControl, IContainsViewModel where T:ViewModelBase
+    {
+        public UserControlWithViewModel()
+        {
+
+        }
+
+        public UserControlWithViewModel(T viewModel)
+        {
+            ViewModel = viewModel;
+        }
+
+        public T ViewModel
+        {
+            get { return (T)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(ViewModelBase), 
+                typeof(UserControlWithViewModel<>), new PropertyMetadata(null));
+
+        public ViewModelBase GetViewModel()
+        {
+            return ViewModel;
+        }
+    }
+
+    internal interface IContainsViewModel
+    {
+        ViewModelBase GetViewModel();
+    }
+}
---
+++ src/Panacea.Core/Mvvm/ViewModelBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Core.Mvvm
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void Activate()
+        {
+
+        }
+
+        public virtual void Deactivate()
+        {
+
+        }
+
+        protected virtual void SetProperty([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
