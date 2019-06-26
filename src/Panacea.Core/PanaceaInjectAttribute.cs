7a08a7caddfed47419dc10a6b920cba2e81dba12
Portno <j.remoundos@dotbydot.gr>
2019-06-26 17:13:32 +0300

    added inject attribute

---
+++ src/Panacea.Core/PanaceaInjectAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Core
+{
+    public class PanaceaInjectAttribute : Attribute
+    {
+        public string Alias { get; set; }
+
+        public string Description { get; set; }
+
+        public string Example { get; set; }
+
+
+        public PanaceaInjectAttribute(string alias, string description, string example)
+        {
+            Alias = alias;
+            Description = description;
+            Example = example;
+        }
+    }
+
+}
+
+
